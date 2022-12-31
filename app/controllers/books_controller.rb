1e42dded376a0b54b22d83ffa943c6df3a100d64
EC2 Default User <ec2-user@ip-172-31-42-242.ap-northeast-1.compute.internal>
2022-12-31 23:09:59 +0000

    [add]3-13 showページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,9 @@ class BooksController < ApplicationController
   def create
 
     book = Book.new(book_params)
-
     book.save
 
-    redirect_to'/top'
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -18,6 +17,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'books/new'
-  get 'top' => 'homes#top'
+  get 'books/edit'
+  get 'top' => 'homes#top0'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
+  get 'books/:id' => 'books#show' , as: 'book'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
